2e9c7ab61599f361e30867c4c6f282c567afb557
relay22 <refcell22@naver.com>
2023-05-04 06:49:40 +0900

    logo 추가

---
+++ src/components/app/main/Logo.svelte
@@ -0,0 +1,26 @@
+<script>
+  import logo from '$lib/images/icon/logo.png'
+</script>
+
+<div class="logo">
+  <img src={logo}/>
+  MapleLife
+</div>
+
+<style lang="scss">
+  .logo{
+    top:10px;
+    left:10px;
+    display: flex;
+    position: sticky;
+    align-items: center;
+    font-size: 18px;
+    font-weight: 700;
+    color: #4d4d4d;
+    img{
+      margin-right: 5px;
+      width: 30px;
+      height: 30px;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
   import Toolbar from "../components/app/main/Toolbar.svelte";
+  import Logo from "../components/app/main/Logo.svelte";
 </script>
 
+<Logo/>
 <Toolbar/>
