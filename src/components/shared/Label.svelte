e7f5d9e000978c1f21a6c3a210028142cf49aed3
relay955 <refcell22@naver.com>
2023-05-04 15:53:15 +0900

    라벨 추가

---
+++ src/components/shared/Label.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let color = 'gray';
+</script>
+<div class="main" style={`background-color:${color}`}>
+  <slot></slot>
+</div>
+
+<style>
+  .main{
+    display: inline-flex;
+    font-size: 13px;
+    font-weight: bold;
+    color: white;
+    border-radius: 20px;
+    width: 60px;
+    height: 20px;
+    padding-bottom: 2px;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
