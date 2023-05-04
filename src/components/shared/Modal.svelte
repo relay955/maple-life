32c70a5b668c9e12810e4e74ab8a51022faa9ccb
relay955 <refcell22@naver.com>
2023-05-04 17:04:41 +0900

    할일 생성 모달 도입

---
+++ src/components/shared/Modal.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+  import Space from "./Space.svelte";
+
+  export let title = "";
+  export let isOpen = false;
+  export let onClose: () => void = ()=>{};
+
+  function onClickOuter(e: MouseEvent){
+    if(e.target !== e.currentTarget) return;
+    onClose();
+  }
+</script>
+
+<div class={`Popup ${isOpen?"opened":""}`}
+            on:mousedown={onClickOuter}>
+  <div class="inner-item">
+    <div style="display:flex; margin-bottom:20px; height:25px">
+      <div class="title">{title}</div>
+      <Space/>
+      <div class="close-button" on:click={onClose}>
+        X
+      </div>
+    </div>
+    <slot></slot>
+  </div>
+</div>
+
+<style lang="scss">
+  .close-icon{
+    width:25px;
+    height:25px;
+  }
+  .Popup {
+    visibility: hidden;
+    position: fixed;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0);
+    transition: 0.3s all;
+    z-index: 10;
+    left: 0;
+    top: 0;
+
+    .inner-item {
+      position: relative;
+      background: rgba(240,240,240);
+      border:1px solid lightgrey;
+      border-radius: 10px;
+      opacity: 0;
+      min-width: 400px;
+      min-height: 130px;
+      transform: scale(0.8);
+      margin: auto;
+      padding: 20px;
+      box-sizing: border-box;
+      transition: 0.3s all;
+
+      .title {
+        font-weight: bold;
+      }
+    }
+
+    &.opened {
+      visibility: visible;
+      background-color: rgba(0,0,0,0.3);
+
+      .inner-item {
+        opacity: 1;
+        transform: scale(1);
+      }
+    }
+
+    .close-button{
+      cursor:pointer;
+      .close-icon{
+        width:25px;
+        height:25px;
+      }
+    }
+  }
+
+</style>
