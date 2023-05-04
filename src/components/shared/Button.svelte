7b3836d7be1969875554a69fb94946a0b61fc83c
relay955 <refcell22@naver.com>
2023-05-04 17:22:54 +0900

    버튼 컴포넌트 추가

---
+++ src/components/shared/Button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let onClick:()=>void;
+
+</script>
+<button on:click={onClick}><slot></slot></button>
+
+<style>
+  button{
+    background-color: #70a5e0;
+    border: none;
+    color: white;
+    padding: 5px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 16px;
+    cursor: pointer;
+    border-radius: 5px;
+    width: 100%;
+    transition: 0.2s all;
+  }
+  button:hover{
+    background-color: #6391c1;
+  }
+</style>
