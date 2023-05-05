c4513ccec5c8e8b98c429d84172306f8af60fb1e
relay22 <refcell22@naver.com>
2023-05-05 16:50:54 +0900

    캐릭터 추가기능 추가

--- src/components/app/main/CharacterEditModal.svelte
+++ src/components/app/main/CharacterEditModal.svelte
@@ -5,12 +5,19 @@ import Modal from "../../shared/Modal.svelte";
 import Button from "../../shared/Button.svelte";
 import type {Todo} from "../../../storage/dto/todo";
 import type {Character} from "../../../storage/dto/character";
+import {Jumper} from "svelte-loading-spinners";
+import {requestMapleCharacterInfo} from "../../../util/mapleParser";
+import {PROXY_URL} from "../../../config";
 
 export let isOpen = false;
+let character:Character = reset()
+let isEditMode = false;
+let isManualMode = false;
+let isParsing = false;
 export let onClose = () => {};
 export let onSubmit = (character:Character) => {};
 
-const reset = ():Character => {
+function reset():Character{
   return {
     name:"",
     classType:"",
@@ -19,17 +26,34 @@ const reset = ():Character => {
   };
 }
 
-let character:Character = reset()
-
-
-const onClickSubmitButton = () => {
-  onSubmit(character);
-  character = reset();
-  onClose();
+const onClickSubmitButton = async () => {
+  try {
+    if (!isManualMode) {
+      isParsing = true;
+      const result = await requestMapleCharacterInfo(character.name, PROXY_URL)
+      character = {
+        name: result.nickname,
+        classType: result.classType,
+        imgUrl: result.imgUrl,
+        level: result.level,
+      }
+    }
+    onSubmit(character);
+    onClose();
+  }catch (e){
+    isManualMode = true;
+  }finally {
+    isParsing = false;
+  }
 }
 </script>
 
 <Modal title="캐릭터 등록" isOpen={isOpen} onClose={onClose}>
   <Input title="닉네임" bind:value={character.name} />
-  <Button onClick={onClickSubmitButton} style="margin-top: 10px">생성</Button>
+  {#if isEditMode || isManualMode}
+    {#if isManualMode}<div>서버에서 데이터를 불러오는데 문제가 발생하여 직업 정보를 수동으로 입력해야합니다.</div>{/if}
+    <Input title="레벨"/>
+    <Input title="직업"/>
+  {/if}
+  <Button onClick={onClickSubmitButton} isLoading={isParsing} style="margin-top: 10px">생성</Button>
 </Modal>
--- src/util/mapleParser.ts
+++ src/util/mapleParser.ts
@@ -6,12 +6,18 @@ interface MapleCharacterInfo{
     detailInfoUrl:string;
     nickname:string;
     classType:string;
-    level:string;
+    level:number;
 }
 export const requestMapleCharacterInfo = async (characterName: string,proxy:string) => {
     let url = `${proxy}/https://maplestory.nexon.com/Ranking/World/Total?c=${characterName}&w=0`
     const res = await axios.get(url)
-    return parseMapleCharacterInfo(res.data)
+    try {
+        return parseMapleCharacterInfo(res.data)
+    }catch(e){
+        let url = `${proxy}/https://maplestory.nexon.com/Ranking/World/Total?c=${characterName}&w=254`
+        const res = await axios.get(url)
+        return parseMapleCharacterInfo(res.data)
+    }
 }
 
 const parseMapleCharacterInfo = (rankingHtmlData:string):MapleCharacterInfo => {
@@ -22,10 +28,11 @@ const parseMapleCharacterInfo = (rankingHtmlData:string):MapleCharacterInfo => {
         imgUrl: tr.querySelector(".left > .char_img > img")!.getAttribute("src")!,
         detailInfoUrl: "https://maplestory.nexon.com"+tr.querySelector(".left > dl > dt > a")!.getAttribute("href")!,
         nickname: tr.querySelector(".left > dl > dt > a")!.textContent,
-        classType: tr.querySelector(".left > dl > dd")!
-            .textContent
+        classType: tr.querySelector(".left > dl > dd")!.textContent
             .split("/")[1]
             .trim(),
-        level: tr.querySelector("td:nth-child(3)")!.textContent
+        level: Number(tr.querySelector("td:nth-child(3)")!.textContent
+            .split(".")[1]
+            .trim())
     }
 }
