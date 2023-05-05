9ab303cb63e1d251ceedb5ad995b3a6357cc40ad
relay22 <refcell22@naver.com>
2023-05-05 13:09:09 +0900

    캐릭터 추가버튼 추가

--- src/components/app/main/Toolbar.svelte
+++ src/components/app/main/Toolbar.svelte
@@ -1,8 +1,16 @@
 <script>
-
+  import MdPersonAdd from 'svelte-icons/md/MdPersonAdd.svelte'
+  import SvelteTooltip from "svelte-tooltip";
 </script>
 
 <div class="toolbar">
+  <div class="icon-button">
+    <SvelteTooltip tip="캐릭터 추가" top>
+      <span class="icon">
+        <MdPersonAdd fill="gray"/>
+      </span>
+    </SvelteTooltip>
+  </div>
 </div>
 
 <style>
@@ -17,5 +25,24 @@
     z-index: 100;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .icon-button{
+    width:35px;
+    height:35px;
+    padding:5px;
+    box-sizing: border-box;
+    border-radius: 30px;
+    color:white;
+    cursor: pointer;
+    transition: 0.2s all;
+  }
+  .icon-button:hover{
+    background-color: lightgray;
+  }
+  .icon{
+    color:gray;
   }
 </style>
