1674631e4f023aec00e72eda96d50ddb3d9ea5ef
relay22 <refcell22@naver.com>
2023-05-05 18:48:15 +0900

    캐릭터 정보 로딩중 못나가게 변경, 디자인약간변경, 캐릭터 수정기능 추가

--- src/components/app/main/CharacterEditModal.svelte
+++ src/components/app/main/CharacterEditModal.svelte
@@ -8,14 +8,18 @@ import {PROXY_URL} from "../../../config";
 import { toast } from '@zerodevx/svelte-toast'
 import NumberInput from "../../shared/NumberInput.svelte";
 import {v4 as uuidv4} from "uuid";
+import IconButton from "../../shared/IconButton.svelte";
+import MdDelete from 'svelte-icons/md/MdDelete.svelte'
 
 export let isOpen = false;
 let character:Character = reset()
-let isEditMode = false;
 let isManualMode = false;
 let isParsing = false;
 export let onClose = () => {};
 export let onSubmit = (character:Character) => {};
+export let isEditMode = false;
+export let editCharacter:Character|undefined = undefined;
+export let onDelete = (character:Character) => {};
 
 function reset():Character{
   return {
@@ -32,8 +36,9 @@ const onClickSubmitButton = async () => {
     if (!isManualMode) {
       isParsing = true;
       const result = await requestMapleCharacterInfo(character.name, PROXY_URL)
+      isParsing = false;
       character = {
-        id: uuidv4(),
+        id: isEditMode ? character.id : uuidv4(),
         name: result.nickname,
         classType: result.classType,
         imgUrl: result.imgUrl,
@@ -44,28 +49,58 @@ const onClickSubmitButton = async () => {
         toast.push("레벨은 1~300 사이입니다.");
         return;
       }
+      character.id = uuidv4()
     }
-    character.id = uuidv4()
     onSubmit(character);
-    onClose();
+    onCloseProxy()
   }catch (e){
     isManualMode = true;
   }finally {
     isParsing = false;
   }
 }
+
+const onClickDeleteButton = () => {
+  onDelete(character);
+  onCloseProxy()
+}
+
+$: {
+  if(editCharacter !== undefined && isEditMode){
+    character = editCharacter
+  }
+}
+
+const onCloseProxy = () => {
+  if(isParsing) {
+    toast.push("캐릭터 정보를 불러오는 중입니다. 잠시만 기다려 주세요.")
+    return;
+  }
+  character = reset();
+  isManualMode = false;
+  onClose()
+}
+
 </script>
 
-<Modal title="캐릭터 등록" isOpen={isOpen} onClose={onClose}>
+<Modal title={isEditMode ? "캐릭터 수정":"캐릭터 등록"} isOpen={isOpen} onClose={onCloseProxy}>
   <Input title="닉네임" bind:value={character.name} />
-  {#if isEditMode || isManualMode}
-    {#if isManualMode}
-      <div class="error-message">서버에서 데이터를 불러오는데 문제가 발생하여<br/>직업 정보를 수동으로 입력해야합니다.</div>
-    {/if}
+  {#if isManualMode}
+    <div class="error-message">서버에서 데이터를 불러오는데 문제가 발생하여<br/>직업 정보를 수동으로 입력해야합니다.</div>
     <NumberInput title="레벨" bind:value={character.level}/>
     <Input title="직업" bind:value={character.classType}/>
   {/if}
-  <Button onClick={onClickSubmitButton} isLoading={isParsing} style="margin-top: 10px">생성</Button>
+  <div class="button-container">
+    <div class="submit-button">
+      <Button onClick={onClickSubmitButton}
+              isLoading={isParsing}>{isEditMode ? "수정":"생성"}</Button>
+    </div>
+    {#if isEditMode}
+      <IconButton onClick={onClickDeleteButton} style="margin-left: 10px" >
+        <MdDelete/>
+      </IconButton>
+    {/if}
+  </div>
 </Modal>
 
 
@@ -75,5 +110,18 @@ const onClickSubmitButton = async () => {
     font-size: 12px;
     margin-top: 5px;
     margin-bottom: 5px;
+  }
+  .button-container{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 10px;
+  }
+  .submit-button{
+    flex-grow: 1;
+  }
+  .delete-button{
+    margin-left: 10px;
   }
 </style>
