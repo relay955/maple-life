2f1fb0a0c8346fcab360fdc870706b06abe2302d
relay22 <refcell22@naver.com>
2023-05-05 18:02:16 +0900

    할일 수정기능 추가

--- src/components/app/main/TodoEditModal.svelte
+++ src/components/app/main/TodoEditModal.svelte
@@ -37,14 +37,14 @@ let todo:Todo = resetTodo()
 
 const onClickSubmitButton = () => {
   todo.isChecked = todo.type === "perCharacter" ? {} : "unchecked";
-  todo.id = uuidv4();
+  if(!isEditMode) todo.id = uuidv4();
   onSubmit(todo);
   todo = resetTodo();
   onClose();
 }
 </script>
 
-<Modal title="할일 생성" isOpen={isOpen} onClose={onClose}>
+<Modal title={isEditMode ? "할일 수정" : "할일 생성"} isOpen={isOpen} onClose={onClose}>
   <Input title="할일 이름" bind:value={todo.name} />
   <Select title="초기화 간격" bind:value={todo.repeatType}>
     <option value="daily">일일</option>
@@ -56,5 +56,5 @@ const onClickSubmitButton = () => {
     <option value="perCharacter">캐릭터 별</option>
     <option value="perAccount">계정 별</option>
   </Select>
-  <Button onClick={onClickSubmitButton} style="margin-top: 10px">생성</Button>
+  <Button onClick={onClickSubmitButton} style="margin-top: 10px">{isEditMode ? "수정":"생성"}</Button>
 </Modal>
