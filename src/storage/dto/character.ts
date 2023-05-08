1106bfc9bde6036649354030ea1b74db92dc538c
yhkim <yh.kim@ncodi.co.kr>
2023-05-08 17:59:26 +0900

    world/account 단위 구성진행중

--- src/storage/dto/character.ts
+++ src/storage/dto/character.ts
@@ -1,7 +1,11 @@
+import type {World} from "./world";
+
 export interface Character {
     id:string;
     imgUrl: string;
     name: string;
     level: number;
     classType:string;
+    world?:World;
+    account?:string;
 }
