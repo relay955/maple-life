0b9bbafc10cd8c1bd5a4466e3ebca8a4f2e9e953
relay955 <refcell22@naver.com>
2023-05-08 17:59:26 +0900

    world/account 단위 구성진행중

--- src/storage/dto/todo.ts
+++ src/storage/dto/todo.ts
@@ -3,7 +3,7 @@ export interface Todo {
     id:string;
     name: string;
     repeatType:"daily"|"weeklyMonday"|"weeklyThursday"|"monthly";
-    type:"perCharacter"|"perAccount";
+    type:"perCharacter"|"perAccount"|"perWorld";
     color:string;
-    isChecked:{[index:string]:TodoCheckType} | TodoCheckType//per character일경우 캐릭터별로, perAccount일경우 단일
+    isChecked:{[index:string]:TodoCheckType} | TodoCheckType//per character일경우 캐릭터별로, perAccount일경우 단일, per world일경우 월드별
 }
