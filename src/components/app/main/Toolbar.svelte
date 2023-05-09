49ed44064a4e70a307471b7cd4b85076eab94e31
relay22 <refcell22@naver.com>
2023-05-09 20:19:27 +0900

    restore toolbar functions

--- src/components/app/main/Toolbar.svelte
+++ src/components/app/main/Toolbar.svelte
@@ -6,11 +6,22 @@
   import SvelteTooltip from "svelte-tooltip";
   import IconButton from "../../shared/IconButton.svelte";
   import type {Settings} from "../../../storage/dto/settings";
+  import {liveQuery} from "dexie";
+  import {idb} from "../../../storage/idb";
 
   export let onClickCharacterAddButton:()=>void;
-  export let onClickShortHeightModeButton:()=>void;
-  export let onClickShowCharacterPreviewButton:()=>void;
-  export let settings:Settings;
+
+  const onClickShortHeightModeButton = () => {
+    idb.settings.update("shortHeightMode", {value: !$shortHeightMode});
+  }
+  const onClickShowCharacterPreviewButton = () => {
+    idb.settings.update("showCharacterPreview", {value: !$showCharacterPreview});
+  }
+
+  let showCharacterPreview =
+    liveQuery(async () => (await idb.settings.get("showCharacterPreview"))?.value);
+  let shortHeightMode =
+    liveQuery(async () => (await idb.settings.get("shortHeightMode"))?.value);
 </script>
 
 <div class="toolbar">
@@ -20,12 +31,12 @@
   </IconButton>
   <IconButton onClick={onClickShortHeightModeButton}
               tooltip="좁은높이 모드 - 한 화면에 더 많은 할일을 보고싶을때 사용해보세요."
-              activated={settings.shortHeightMode}>
+              activated={$shortHeightMode}>
     <MdUnfoldLess/>
   </IconButton>
   <IconButton onClick={onClickShowCharacterPreviewButton}
               tooltip="캐릭터 사진 보기 - 한 화면에 더 많은 할일을 보고싶다면 끌 수 있어요."
-              activated={settings.showCharacterPreview}>
+              activated={$showCharacterPreview}>
     <MdPhotoSizeSelectLarge/>
   </IconButton>
 </div>
