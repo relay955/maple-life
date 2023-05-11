46b1d64d29b28ff5afc4e26a5948a8f58559d712
relay22 <refcell22@naver.com>
2023-05-11 22:59:17 +0900

    페이지 라우팅/레이아웃 분리

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Logo from "../components/shared/Logo.svelte";
+  import LeftBar from "../components/app/main/LeftBar.svelte";
+  import {SvelteToast} from "@zerodevx/svelte-toast";
+  import {onMount} from "svelte";
+  import {runOnInitialize} from "../logic/runOnInitialize";
+
+  onMount(runOnInitialize)
+
+</script>
+
+<Logo isFixed/>
+<LeftBar/>
+
+<div class="main">
+  <slot></slot>
+</div>
+
+<SvelteToast/>
+
+<style>
+  .main{
+    width:100%;
+    text-align: center;
+    padding-bottom: 400px;
+  }
+</style>
---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+  import Toolbar from "../../components/app/main/Toolbar.svelte";
+  import type {Character} from "../../storage/dto/character";
+  import type {Todo} from "../../storage/dto/todo";
+  import {onMount} from "svelte";
+  import TodoEditModal from "../../components/app/main/TodoEditModal.svelte";
+  import CharacterEditModal from "../../components/app/main/CharacterEditModal.svelte";
+  import TodoHeader from "../../components/app/main/TodoHeader.svelte";
+  import AddTodoButton from "../../components/app/main/AddTodoButton.svelte";
+  import TodoList from "../../components/app/main/TodoList.svelte";
+  import type {Account} from "../../storage/dto/account";
+  import AccountEditModal from "../../components/app/main/AccountEditModal.svelte";
+  import {runOnInitialize} from "../../logic/runOnInitialize";
+
+  export const prerender = true;
+  export const ssr = true;
+
+  let isEditTodoModalOpen = false;
+  let editTodoModalEditMode = false;
+  let editTodoModalTarget:Todo|undefined = undefined;
+
+  let isEditCharacterModalOpen = false;
+  let isEditCharacterModalEditMode = false;
+  let editCharacterModalTarget:Character|undefined = undefined;
+
+  let isEditAccountModalOpen = false;
+  let isEditAccountModalEditMode = false;
+  let isEditAccountModalTarget:Account|undefined = undefined;
+
+  const onClickAddTodoButton = () => {
+    isEditTodoModalOpen = true;
+    editTodoModalEditMode = false;
+    editTodoModalTarget = undefined;
+  }
+
+  const onClickEditTodoButton = (todo:Todo) => {
+    isEditTodoModalOpen = true;
+    editTodoModalEditMode = true;
+    editTodoModalTarget = todo;
+  }
+
+  const onClickAddCharacterButton = () => {
+    isEditCharacterModalOpen = true;
+    isEditCharacterModalEditMode = false;
+    editCharacterModalTarget = undefined;
+  }
+
+  const onClickAddAccountButton = () => {
+    isEditAccountModalOpen = true;
+    isEditAccountModalEditMode = false;
+    isEditAccountModalTarget = undefined;
+  }
+
+  const onClickEditCharacter = (character:Character) => {
+    isEditCharacterModalOpen = true;
+    isEditCharacterModalEditMode = true;
+    editCharacterModalTarget = character;
+  }
+
+  const onClickEditAccount = (account:Account) => {
+    isEditAccountModalOpen = true;
+    isEditAccountModalEditMode = true;
+    isEditAccountModalTarget = account;
+  }
+
+</script>
+
+<Toolbar onClickCharacterAddButton={onClickAddCharacterButton}
+         onClickAccountAddButton={onClickAddAccountButton}/>
+
+<div class="container">
+  <TodoHeader onClickCharacter={onClickEditCharacter} onClickAccountBar={onClickEditAccount}/>
+  <TodoList onClickEditTodoButton={onClickEditTodoButton}/>
+  <AddTodoButton onClick={onClickAddTodoButton}/>
+</div>
+
+<TodoEditModal isOpen={isEditTodoModalOpen}
+               isEditMode={editTodoModalEditMode}
+               editTodo={editTodoModalTarget}
+               onClose={()=>isEditTodoModalOpen = false}/>
+<CharacterEditModal isOpen={isEditCharacterModalOpen}
+                    isEditMode={isEditCharacterModalEditMode}
+                    editCharacter={editCharacterModalTarget}
+                    onClose={()=>isEditCharacterModalOpen = false}/>
+<AccountEditModal isOpen={isEditAccountModalOpen}
+                    isEditMode={isEditAccountModalEditMode}
+                    editAccount={isEditAccountModalTarget}
+                    onClose={()=>isEditAccountModalOpen = false}/>
+
+<style lang="scss">
+  .container{
+    text-align: left;
+    display: inline-block;
+    min-width: 1420px;
+    margin-left: 60px;
+    margin-right: 20px;
+    padding-bottom: 400px;
+  }
+  @media(max-width: 1550px){
+    .container{
+      min-width: calc(100% - 70px);
+    }
+  }
+
+</style>
+
