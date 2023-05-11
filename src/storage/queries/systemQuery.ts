018e54fe95b1c28698c3eab2ec8ef6887119cce8
relay22 <refcell22@naver.com>
2023-05-11 23:36:27 +0900

    toolbar 리팩터링

--- src/storage/queries/systemQuery.ts
+++ src/storage/queries/systemQuery.ts
@@ -8,7 +8,23 @@ export const updateLastUpdatedTime = async () => {
         value: moment().format("YYYY-MM-DD")
     })
 }
+export const updateShortHeightMode = async (value: boolean) => {
+    await idb.settings.put({
+        name: "shortHeightMode",
+        value: value
+    })
+}
+
+export const updateShowCharacterPreview = async (value: boolean) => {
+    await idb.settings.put({
+        name: "showCharacterPreview",
+        value: value
+    })
+}
 
 
 export const lqShowCharacterPreview =
     liveQuery(async () => (await idb.settings.get("showCharacterPreview"))?.value);
+export const lqShortHeightMode =
+    liveQuery(async () => (await idb.settings.get("shortHeightMode"))?.value);
+
