e34a7094101b90cea9d8f2c0227005a874ae9b83
relay22 <refcell22@naver.com>
2023-05-11 23:36:57 +0900

    toolbar 리팩터링

--- src/components/app/_todo/Toolbar.svelte
+++ src/components/app/_todo/Toolbar.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
   import MdPersonAdd from 'svelte-icons/md/MdPersonAdd.svelte'
   import MdUnfoldLess from 'svelte-icons/md/MdUnfoldLess.svelte'
-  import MdPhotoSizeSelectLarge from 'svelte-icons/md/MdPhotoSizeSelectLarge.svelte'
+  import MdPhotoSizeSelectLarge
+    from 'svelte-icons/md/MdPhotoSizeSelectLarge.svelte'
   import MdGroupAdd from 'svelte-icons/md/MdGroupAdd.svelte'
   //@ts-ignore
   import SvelteTooltip from "svelte-tooltip";
   import IconButton from "../../shared/basicComponent/IconButton.svelte";
   import {
     lqShortHeightMode,
-    lqShowCharacterPreview, updateShortHeightMode, updateShowCharacterPreview
+    lqShowCharacterPreview,
+    updateShortHeightMode,
+    updateShowCharacterPreview
   } from "../../../storage/queries/systemQuery";
 
   export let onClickAccountAddButton:()=>void;
   export let onClickCharacterAddButton:()=>void;
 
-  const onClickShortHeightModeButton = () => updateShortHeightMode(!$lqShortHeightMode)
-  const onClickShowCharacterPreviewButton = () => updateShowCharacterPreview(!$lqShowCharacterPreview)
+
 
 </script>
 
@@ -27,12 +29,12 @@
   <IconButton onClick={onClickAccountAddButton} tooltip="계정 추가 - 메이플스토리 계정을 여러개 사용하는 경우에만 추가하세요.">
     <MdGroupAdd/>
   </IconButton>
-  <IconButton onClick={onClickShortHeightModeButton}
+  <IconButton onClick={() => updateShortHeightMode(!$lqShortHeightMode)}
               tooltip="좁은높이 모드 - 한 화면에 더 많은 할일을 보고싶을때 사용해보세요."
               activated={$lqShortHeightMode}>
     <MdUnfoldLess/>
   </IconButton>
-  <IconButton onClick={onClickShowCharacterPreviewButton}
+  <IconButton onClick={() => updateShowCharacterPreview(!$lqShowCharacterPreview)}
               tooltip="캐릭터 사진 보기 - 한 화면에 더 많은 할일을 보고싶다면 끌 수 있어요."
               activated={$lqShowCharacterPreview}>
     <MdPhotoSizeSelectLarge/>
