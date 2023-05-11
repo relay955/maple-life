1ebc8ab6ecb8b269ac05ea20d101786962ece0e7
relay22 <refcell22@naver.com>
2023-05-11 23:26:53 +0900

    todoheader livequery 스토리지쪽으로 이동

--- src/storage/queries/systemQuery.ts
+++ src/storage/queries/systemQuery.ts
@@ -1,11 +1,14 @@
 import {idb} from "../idb";
 import moment from "moment";
+import {liveQuery} from "dexie";
 
-export const systemQuery = {
-    updateLastUpdatedTime:() => {
-        idb.systemInfo.put({
-            name: "lastUpdated",
-            value: moment().format("YYYY-MM-DD")
-        })
-    }
+export const updateLastUpdatedTime = async () => {
+    await idb.systemInfo.put({
+        name: "lastUpdated",
+        value: moment().format("YYYY-MM-DD")
+    })
 }
+
+
+export const lqShowCharacterPreview =
+    liveQuery(async () => (await idb.settings.get("showCharacterPreview"))?.value);
