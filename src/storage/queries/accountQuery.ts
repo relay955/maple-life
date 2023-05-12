ecf9d78822e379ff41cde71c183460e6dacd724a
relay22 <refcell22@naver.com>
2023-05-12 13:06:47 +0900

    캐릭터를 수동으로 변경했을때 해당 월드에 캐릭터가 없어질경우 월드가 삭제되도록 변경, 월드/계정 저장시 해당 월드/계정에 속한 캐릭터/월드는 저장되지 않도록 수정

--- src/storage/queries/accountQuery.ts
+++ src/storage/queries/accountQuery.ts
@@ -3,14 +3,20 @@ import type {Account} from "../dto/account";
 import {toast} from "@zerodevx/svelte-toast";
 
 export const swapAccountOrder = async (account: Account, targetAccount: Account) => {
-    const temp = account.order
-    account.order = targetAccount.order
-    targetAccount.order = temp;
+    let account1: Account = JSON.parse(JSON.stringify(account))
+    let account2: Account = JSON.parse(JSON.stringify(targetAccount))
+    const temp = account1.order
+    account1.order = account2.order
+    account2.order = temp;
 
-    await idb.account.bulkPut([account, targetAccount])
+    //저장시에는 계정에 포함된 월드는 저장하면 안됨
+    account1.worlds = undefined;
+    account2.worlds = undefined;
+    await idb.account.bulkPut([account1, account2])
 }
 
 export const putAccount = async (account: Account) => {
+    account = JSON.parse(JSON.stringify(account))
     const isFirstCreate = account.id === undefined ||
         (await idb.todo.get(account.id)) === undefined
 
@@ -20,6 +26,8 @@ export const putAccount = async (account: Account) => {
             if (item.order > account.order) account.order = item.order
         })
         account.order ++;
+    }else{
+        account.worlds = undefined;
     }
     idb.account.put(account)
 }
@@ -35,6 +43,13 @@ export const deleteAccount = async (account: Account) => {
     if (deleteTargetCharactersId.length === await idb.character.count()) {
         throw new Error("해당 계정이 삭제되면 모든 캐릭터가 제거되므로 삭제할 수 없습니다.");
     }
+
+    const deleteTargetWorldsId = await idb.accountWorld
+        .where("accountId")
+        .equals(account.id!)
+        .primaryKeys()
+
     await idb.character.bulkDelete(deleteTargetCharactersId)
+    await idb.accountWorld.bulkDelete(deleteTargetWorldsId)
     await idb.account.delete(account.id!)
 }
