b1c53697c71f5bf53e461db36bfb2a0a50efbcce
relay955 <relay955@users.noreply.github.com>
2023-05-12 05:17:10 +0000

    deploy: 81f9535376f05b0724d5983a47d032d293cfeda3

--- _app/immutable/nodes/2.94736c7c.js
+++
@@ -1,1 +0,0 @@
-import{S as n,i as s,s as a,o as e}from"../chunks/index.21926fd8.js";import{j as i}from"../chunks/singletons.d6ba9c45.js";const c=i("goto");async function r(o,t){await c(`/${o}`,{replaceState:t})}function u(o){return e(async()=>{await r("todo")}),[]}class f extends n{constructor(t){super(),s(this,t,u,null,a,{})}}export{f as component};
---
+++ _app/immutable/nodes/2.c205fa63.js
@@ -0,0 +1,1 @@
+import{S as n,i as s,s as a,o as e}from"../chunks/index.21926fd8.js";import{j as i}from"../chunks/singletons.ba22f46e.js";const c=i("goto");async function r(o,t){await c(`/${o}`,{replaceState:t})}function u(o){return e(async()=>{await r("todo")}),[]}class f extends n{constructor(t){super(),s(this,t,u,null,a,{})}}export{f as component};
