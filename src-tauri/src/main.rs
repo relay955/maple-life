596498f54724a86a58e1aa4558519255474b10f7
yhkim <yh.kim@ncodi.co.kr>
2023-05-19 17:33:35 +0900

    tauri 앱에서 별도 프록시를 사용하도록 백엔드 어댑터 추가

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
 fn main() {
   tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![say_hello])
+      .invoke_handler(tauri::generate_handler![request_with_proxy])
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
 }
 
 
 #[tauri::command]
-fn say_hello(name: &str) -> String {
-  format!("hello,{}!",name)
+fn request_with_proxy(url: &str) -> Result<String, String> {
+  println!("{}",format!("proxy detected. {url}"));
+  match reqwest::blocking::get(url.to_string()) {
+    Ok(res) => Ok(res.text().unwrap()),
+    Err(e) => Err(e.to_string()),
+  }
 }
