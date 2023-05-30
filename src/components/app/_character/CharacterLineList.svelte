fc7a6d755c8f2a0f542dfc5a1571925a1984c561
yhkim <yh.kim@ncodi.co.kr>
2023-05-30 16:42:11 +0900

    캐릭터라인에 무보엠 라인수 추가

--- src/components/app/_character/CharacterLineList.svelte
+++ src/components/app/_character/CharacterLineList.svelte
@@ -8,20 +8,16 @@ import {
 import {toast} from "@zerodevx/svelte-toast";
 import DragDropList from "../../shared/DragDropList.svelte";
 import type {EquipmentType} from "../../../util/mapleParser/mapleStat";
-import {
-  summarizeSpec
-} from "../../../logic/specCalculator";
-import {equipmentSetOptions} from "../../../infoDictionary/EquipmentDict";
 import EquipmentCard from "./CharacterLineList/EquipmentCard.svelte";
 import CharacterBasicCard from "./CharacterLineList/CharacterBasicCard.svelte";
 import SummaryCard from "./CharacterLineList/SummaryCard.svelte";
+import IconButton from "../../shared/basicComponent/IconButton.svelte";
+import MdRefresh from 'svelte-icons/md/MdRefresh.svelte'
 
 let filteredCharacter = [];
 
-//무보엠은 따로
 const equipmentTypeOrder:EquipmentType[] = [
-  "반지1","반지2","반지3","반지4","포켓아이템","펜던트1","펜던트2","벨트","얼굴장식",
-  "눈장식","귀고리",
+  "반지1","반지2","반지3","반지4","포켓아이템","펜던트1","펜던트2","벨트","얼굴장식", "눈장식","귀고리",
   "무기","보조무기","엠블렘","모자","상의","하의","신발","어깨장식","장갑","뱃지","훈장"
 ]
 
@@ -32,6 +28,14 @@ $:{
 }
 
 const onClickCharacterCard = async (character: Character) => {
+  try {
+    await requestMapleCharacterDetailInfo(character);
+  }catch(e){
+    toast?.push(e.message)
+  }
+}
+
+const onClickRefreshCharacter = async (character: Character) => {
   try {
     await requestMapleCharacterDetailInfo(character);
   }catch(e){
@@ -59,6 +63,11 @@ const onMoveCharacter = (event) => {
           스텟정보 갱신 필요
         </div>
       {/if}
+      <IconButton style="margin-left: 10px; margin-right: 10px;"
+                  onClick={()=>onClickRefreshCharacter(character)}
+      >
+        <MdRefresh/>
+      </IconButton>
     </div>
   </DragDropList>
 </div>
