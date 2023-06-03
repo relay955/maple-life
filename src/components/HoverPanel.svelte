d283ac50864f420f00c8ac0ee0099782c203a6d4
relay955 <refcell22@naver.com>
2023-06-03 19:33:34 +0900

    버프스킬 호버패널 설명 추가

---
+++ src/components/HoverPanel.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let width = 300;
+  export let x = 0;
+  export let y = 0;
+  export let isVisible;
+</script>
+
+{#if isVisible}
+  <div class="main"
+       style={`width:${width}px; top:${y+10}px; left:${x+10}px`}>
+    <slot></slot>
+  </div>
+{/if}
+
+<style>
+  .main{
+    position:absolute;
+    backdrop-filter: blur(4px);
+    background: rgba(255,255,255,0.5);
+    z-index: 1000;
+    padding:10px;
+    border-radius: 5px;
+    border:solid 1px #cccccc;
+  }
+</style>
--- src/routes/character/[id]/stat/+page.svelte
+++ src/routes/character/[id]/stat/+page.svelte
@@ -3,16 +3,36 @@
   import {idb} from "../../../../storage/idb";
   import {page} from "$app/stores";
   import {isOnTauri} from "../../../../backendAdapter/backendAdapter";
+  import type {Buff} from "../../../../infoDictionary/BuffDict";
   import {buffDict} from "../../../../infoDictionary/BuffDict";
   import {simulate, summarizeSpec} from "../../../../logic/specCalculator";
+  import HoverPanel from "../../../../components/HoverPanel.svelte";
+  import type {LinkSkill} from "../../../../infoDictionary/LinkSkillDict";
 
   export let character = liveQuery(() => idb.character.get(Number($page.params.id)))
+
 
   const debug = () => {
     //@ts-ignore
     let specSummary = summarizeSpec($character, $character.spec.default)
     simulate(specSummary)
   }
+
+  //hoverPanel
+  let hoveredItem:Buff | LinkSkill;
+  let hoveredItemName:string;
+  let hoverPanelX = 0;
+  let hoverPanelY = 0;
+  let isVisibleHoverPanel = false;
+
+  const onMouseMove = (e:MouseEvent,name:string,item:Buff|LinkSkill) =>{
+    hoverPanelX = e.clientX;
+    hoverPanelY = e.clientY;
+    isVisibleHoverPanel = true;
+    hoveredItemName = name;
+    hoveredItem=item;
+  }
+  const onMouseLeave = (e:MouseEvent) => isVisibleHoverPanel = false
 
 </script>
 {#if isOnTauri() && $character}
@@ -23,6 +43,8 @@
       {#each Object.keys(buffDict) as buffName}
         {@const buff = buffDict[buffName]}
         <img src={buff.imgUrl}
+             on:mousemove={(e)=>onMouseMove(e,buffName,buff)}
+             on:mouseleave={onMouseLeave}
              class:active={$character.spec.default.buff[buffName] !== undefined} />
       {/each}
     </div>
@@ -32,6 +54,11 @@
     <button on:click={debug}>디버그</button>
   </div>
 </div>
+  <HoverPanel isVisible={isVisibleHoverPanel}
+              x={hoverPanelX} y={hoverPanelY}>
+    <div class="hoverpanel-title">{hoveredItemName}</div>
+    <div class="hoverpanel-description">{hoveredItem.description ?? "설명 없음"}</div>
+  </HoverPanel>
 {/if}
 
 <style lang="scss">
@@ -47,9 +74,20 @@
     img{
       margin-right: 3px;
       filter:grayscale(1); opacity: (0.5);
+      cursor:pointer;
     }
     img.active{
       filter: brightness(0.5);
     }
+  }
+  .hoverpanel-title{
+    font-size:14px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  .hoverpanel-description{
+    font-size: 12px;
+    color: #5e5e5e;
+    white-space: pre-line;
   }
 </style>
