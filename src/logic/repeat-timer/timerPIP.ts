94430940d50d2b1e68526baeff69a52d7e6181a9
relay22 <refcell22@naver.com>
2025-08-30 22:10:58 +0900

    플로팅 타이머 구축

--- src/logic/repeat-timer/timerPIP.ts
+++ src/logic/repeat-timer/timerPIP.ts
@@ -1,13 +1,15 @@
 export const openFloatingTimerPip = async (window:Window) => {
+  //@ts-ignore
   if (!window.documentPictureInPicture) {
     alert("해당 기능을 지원하지 않는 브라우저입니다. (Chrome,Edge 사용 가능)");
     return;
   }
 
   try {
+    //@ts-ignore
     const pipWindow = await window.documentPictureInPicture.requestWindow({
       width: 400,
-      height: 70,
+      height: 50,
     });
 
     // 스타일 추가
@@ -15,26 +17,35 @@ export const openFloatingTimerPip = async (window:Window) => {
     style.textContent = `
       body {
         margin: 0;
-        padding: 20px;
+        padding-left: 20px;
+        padding-right: 10px;
+        height: 100vh;
         font-family: Arial, sans-serif;
         background: #000;
         color: #fff;
         display: flex;
-        flex-direction: column;
         align-items: center;
         justify-content: center;
-        height: 100vh;
         box-sizing: border-box;
       }
-      .timer-display {
-        font-size: 24px;
-        font-weight: bold;
-        text-align: center;
+      .timer-bar {
+        flex:1;
+        height: 20px;
+        background-color: #333;
+        border-radius: 10px;
+        overflow: hidden;
+        margin-right:10px;
       }
-      .status {
-        font-size: 14px;
-        margin-top: 10px;
-        opacity: 0.8;
+      .progress {
+        width: 0%;
+        height: 100%;
+        background-color: #44aaee;
+        transition: width 0.2s linear;
+      }
+      .timer-text {
+        font-size: 18px;
+        font-weight: bold;
+        min-width: 60px;
       }
     `;
     pipWindow.document.head.appendChild(style);
@@ -42,16 +53,29 @@ export const openFloatingTimerPip = async (window:Window) => {
 
     // 타이머 콘텐츠 추가
     pipWindow.document.body.innerHTML = `
-      <div class="timer-display" id="timer">
-        1
+      <div class="timer-bar">
+        <div class="progress" id="timer"></div>
       </div>
-      <div class="status" id="status">
-        2
-      </div>
+      <div class="timer-text" id="timer-text">0초</div>
     `;
     return pipWindow;
 
   } catch (error) {
     alert("플로팅 타이머를 열 수 없습니다.");
+  }
+}
+
+
+export const updateLeftSecond = (pipWindow:any, maxTick:number,  leftTick:number) => {
+  const timer = pipWindow.document.getElementById('timer');
+  const timerText = pipWindow.document.getElementById('timer-text');
+  
+  if (timer && timerText) {
+    if (maxTick === 0) maxTick = 1;
+    timer.style.width = `${Math.min((leftTick/maxTick)*100,100)}%`;
+    timerText.textContent = `${Math.floor(leftTick/10)}초`;
+    if (leftTick === 0) {
+      timerText.textContent = '미탐지';
+    }
   }
 }
