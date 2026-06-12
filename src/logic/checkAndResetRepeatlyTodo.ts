b67d9511e8837a679a71fa0a0b0f1b93b4b961f9
relay955 <refcell22@naver.com>
2026-06-13 00:14:03 +0900

    [dist] 초기화 버그 수정

--- src/logic/checkAndResetRepeatlyTodo.ts
+++ src/logic/checkAndResetRepeatlyTodo.ts
@@ -9,12 +9,26 @@ export const checkAndResetRepeatlyTodo = async () => {
 
     if (today.isAfter(lastUpdated)) {
         console.log("date changed. reset repeatly todo")
+
+        const shouldResetDaily = true;
+
+        const mostRecentMonday = moment(today).startOf('isoWeek');
+        const shouldResetWeeklyMonday = lastUpdated.isBefore(mostRecentMonday);
+
+        const mostRecentThursday = today.isoWeekday() >= 4
+            ? moment(today).isoWeekday(4).startOf('day')
+            : moment(today).isoWeekday(4).subtract(1, 'week').startOf('day');
+        const shouldResetWeeklyThursday = lastUpdated.isBefore(mostRecentThursday);
+
+        const mostRecentMonthStart = moment(today).startOf('month');
+        const shouldResetMonthly = lastUpdated.isBefore(mostRecentMonthStart);
+
         //일퀘/월요일주간퀘/목요일주간퀘/월간퀘 초기화
         idb.todo.toCollection().modify(todo => {
-            if (todo.repeatType === "daily" ||
-                (todo.repeatType === "weeklyMonday" && today.day() === 1) ||
-                (todo.repeatType === "weeklyThursday" && today.day() === 4) ||
-                (todo.repeatType === "monthly" && today.date() === 1)) {
+            if ((todo.repeatType === "daily" && shouldResetDaily) ||
+                (todo.repeatType === "weeklyMonday" && shouldResetWeeklyMonday) ||
+                (todo.repeatType === "weeklyThursday" && shouldResetWeeklyThursday) ||
+                (todo.repeatType === "monthly" && shouldResetMonthly)) {
 
                 Object.keys(todo.isChecked as object).forEach(key =>
                     todo.isChecked[key] = todo.isChecked[key] === "blocked" ? "blocked" : "unchecked"
