4751236a703b77e03ef1847ecf95eee189a41821
relay955 <refcell22@naver.com>
2026-06-13 07:38:14 +0900

    `cachedLastUpdated`를 문자열에서 `moment` 객체로 변경

--- src/logic/checkAndResetRepeatlyTodo.ts
+++ src/logic/checkAndResetRepeatlyTodo.ts
@@ -2,44 +2,39 @@ import moment from "moment";
 import {idb} from "../storage/idb";
 import {updateLastUpdatedTime} from "../storage/queries/systemQuery";
 
-let cachedLastUpdated: string | null = null;
+let cachedLastUpdated: moment.Moment | null = null;
 let isResetting = false;
 
 export const checkAndResetRepeatlyTodo = async () => {
     if (isResetting) return;
 
     const today = moment().startOf('day');
-    const todayStr = today.format("YYYY-MM-DD");
 
-    if (cachedLastUpdated === todayStr) return; 
+    if (cachedLastUpdated && cachedLastUpdated.isSame(today, 'day')) return; 
     isResetting = true;
 
     try {
-        let lastUpdated: moment.Moment;
-        if (cachedLastUpdated !== null) {
-            lastUpdated = moment(cachedLastUpdated);
-        } else {
+        if (cachedLastUpdated === null) {
             const lastUpdatedinDb = await idb.systemInfo.get("lastUpdated");
             const lastUpdatedStr = lastUpdatedinDb?.value ?? "2020-01-01";
-            cachedLastUpdated = lastUpdatedStr;
-            lastUpdated = moment(lastUpdatedStr);
+            cachedLastUpdated = moment(lastUpdatedStr).startOf('day');
         }
 
-        if (today.isAfter(lastUpdated)) {
+        if (today.isAfter(cachedLastUpdated)) {
             console.log("date changed. reset repeatly todo")
 
             const shouldResetDaily = true;
 
             const mostRecentMonday = moment(today).startOf('isoWeek');
-            const shouldResetWeeklyMonday = lastUpdated.isBefore(mostRecentMonday);
+            const shouldResetWeeklyMonday = cachedLastUpdated.isBefore(mostRecentMonday);
 
             const mostRecentThursday = today.isoWeekday() >= 4
                 ? moment(today).isoWeekday(4).startOf('day')
                 : moment(today).isoWeekday(4).subtract(1, 'week').startOf('day');
-            const shouldResetWeeklyThursday = lastUpdated.isBefore(mostRecentThursday);
+            const shouldResetWeeklyThursday = cachedLastUpdated.isBefore(mostRecentThursday);
 
             const mostRecentMonthStart = moment(today).startOf('month');
-            const shouldResetMonthly = lastUpdated.isBefore(mostRecentMonthStart);
+            const shouldResetMonthly = cachedLastUpdated.isBefore(mostRecentMonthStart);
 
             //일퀘/월요일주간퀘/목요일주간퀘/월간퀘 초기화
             await idb.todo.toCollection().modify(todo => {
@@ -55,7 +50,7 @@ export const checkAndResetRepeatlyTodo = async () => {
             })
             await updateLastUpdatedTime()
         }
-        cachedLastUpdated = todayStr;
+        cachedLastUpdated = today;
     } finally {
         isResetting = false;
     }
